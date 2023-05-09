58e9f90cd46b7c34e7dc1d93f348108b5bf1b13c
Nadav Hammer <twindata36@gmail.com>
2023-05-09 13:40:37 +0300

    feat: make zod error readable

--- src/utils/express/error.ts
+++ src/utils/express/error.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
+import { fromZodError } from 'zod-validation-error';
 import { ServiceError } from '../errors';
 
 export const errorMiddleware = (error: Error, _req: Request, res: Response, next: NextFunction) => {
@@ -7,7 +8,7 @@ export const errorMiddleware = (error: Error, _req: Request, res: Response, next
     if (error instanceof ZodError) {
         res.status(400).send({
             type: error.name,
-            message: error.message,
+            message: fromZodError(error).message,
         });
     } else if (error instanceof ServiceError) {
         res.status(error.code).send({
