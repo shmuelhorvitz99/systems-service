5eb44b41e628a316169082475f604b1b6b83ba5d
Nadav Hammer <twindata36@gmail.com>
2023-11-29 13:14:51 +0200

    feat: update mongoose to version 8

--- src/express/features/manager.ts
+++ src/express/features/manager.ts
@@ -4,15 +4,17 @@ import { FeaturesModel } from './model.js';
 
 export class FeaturesManager {
     static getByQuery = async (query: Partial<Feature>, step: number, limit?: number): Promise<FeatureDocument[]> => {
-        return FeaturesModel.find(query, {}, limit ? { limit, skip: limit * step } : {}).exec();
+        return FeaturesModel.find(query, {}, limit ? { limit, skip: limit * step } : {})
+            .lean()
+            .exec();
     };
 
     static getCount = async (query: Partial<Feature>): Promise<number> => {
-        return FeaturesModel.count(query);
+        return FeaturesModel.countDocuments(query).lean().exec();
     };
 
     static getById = async (featureId: string): Promise<FeatureDocument> => {
-        return FeaturesModel.findById(featureId).orFail(new DocumentNotFoundError(featureId)).exec();
+        return FeaturesModel.findById(featureId).orFail(new DocumentNotFoundError(featureId)).lean().exec();
     };
 
     static createOne = async (feature: Feature): Promise<FeatureDocument> => {
@@ -20,10 +22,10 @@ export class FeaturesManager {
     };
 
     static updateOne = async (featureId: string, update: Partial<Feature>): Promise<FeatureDocument> => {
-        return FeaturesModel.findByIdAndUpdate(featureId, update, { new: true }).orFail(new DocumentNotFoundError(featureId)).exec();
+        return FeaturesModel.findByIdAndUpdate(featureId, update, { new: true }).orFail(new DocumentNotFoundError(featureId)).lean().exec();
     };
 
     static deleteOne = async (featureId: string): Promise<FeatureDocument> => {
-        return FeaturesModel.findByIdAndDelete(featureId).orFail(new DocumentNotFoundError(featureId)).exec();
+        return FeaturesModel.findByIdAndDelete(featureId).orFail(new DocumentNotFoundError(featureId)).lean().exec();
     };
 }
--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,11 +1,11 @@
 import { ClientSession, connection } from 'mongoose';
 
 export const transaction = async <T, Func extends (session: ClientSession) => Promise<T>>(func: Func): Promise<T> => {
-    let ret;
+    let ret: T | undefined;
 
     await connection.transaction(async (session) => {
         ret = await func(session);
     });
 
-    return ret;
+    return ret!;
 };
