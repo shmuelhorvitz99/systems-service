59b93e1f165be86abddd6c282204e48010baa08e
shmuelhorvitz99 <shmuelhorvitz.yesodot@gmail.com>
2025-02-27 11:37:34 +0200

    A

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import 'dotenv/config';
 import env from 'env-var';
 
@@ -7,19 +6,7 @@ export const config = {
         port: env.get('PORT').default(8000).required().asPortNumber(),
     },
     mongo: {
-        uri: env.get('MONGO_URI').default('mongodb://mongo:27017/mydatabase').required().asString(),
+        uri: env.get('MONGO_URI').default('mongodb://localhost/WhatsDown').required().asString(),
         systemsCollectionName: env.get('SYSTEMS_COLLECTION_NAME').default('systems').required().asString(),
     },
 };
-
-// התחברות למונגו
-mongoose.connect(config.mongo.uri)
-    .then(() => {
-        console.log('MongoDB connected');
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-    });
-
-
-    
